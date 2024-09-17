b809ae3329b7ad95591fcef77f656dcd4aecc853
DesarrolloPLS <desarrollo@plasalle.mx>
2024-09-17 17:43:46 -0600

    se termina el enunciado if de desicion y switch

---
+++ IfDemos2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfDemos2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (5 == 5)
+            {
+                Console.WriteLine("si es mayor");
+            }
+            else
+            {
+                Console.WriteLine("no es mayor");
+            }
+
+            bool mayorEdad  = false;
+            int costoEntrada = 0;
+            if (mayorEdad == true)
+            {
+                Console.WriteLine("es mayor de edad");
+                costoEntrada = 20;
+            }
+            else
+            {
+                Console.WriteLine("no lo es");
+                costoEntrada = 10;
+            }
+
+            Console.WriteLine(costoEntrada);
+
+            Console.ReadLine();
+        }
+    }
+}
