9e7bafd86e2d5fc7a827b40bbe6074c805dcf2d4
DesarrolloPLS <desarrollo@plasalle.mx>
2024-09-30 09:54:05 -0600

    se termina clases y objetos

---
+++ ClasesDemo1/Matematicas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesDemo1
+{
+    static class Matematicas
+    {
+        public static int contador;
+
+        public static double Sumar(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public static double Restar(double n1, double n2)
+        {
+            return n1 - n2; 
+        }
+    }
+}
---
+++ ClasesDemo1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ClasesDemo1
+{
+    // classs nombre de clase
+    // {
+    // }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rectangulo rectangulo = new Rectangulo(23,50);
+            //Rectangulo rectangulo2 = new Rectangulo();
+
+            //double resultadoArea = rectangulo.CalcularArea();
+            //double resultadoPerimetro = rectangulo.CalcularPerimetro();
+            //double resultadoPerimetoEstatico = Rectangulo.CalcularPerimetoEstatico(10, 10);
+
+            //(var baseRect, var alturaRec) = rectangulo;
+
+            //Console.WriteLine(baseRect);
+            //Console.WriteLine(alturaRec);
+
+            //Console.WriteLine(resultadoPerimetoEstatico);
+
+            Circulo circulo1 = new Circulo(30);
+            Circulo circulo2 = new Circulo(40);
+            Circulo circulo3 = new Circulo(50);
+
+            WriteLine("sdasdasd");
+
+            var objetoAnonimo = new
+            {
+                area = 3, campo2 = 5
+            };
+
+            Console.WriteLine(objetoAnonimo.area);
+
+            Console.WriteLine(Circulo.numeroDeCirculos);
+           
+            Console.ReadLine();
+
+        }
+    }
+}
